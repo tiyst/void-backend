ef9518a9f9cda2034ccc1a623aa0e0339fca347e
Yomoboshi <yomoboshi2@gmail.com>
2024-09-16 12:16:59 +0200

    1st fix to build & run

--- src/main/java/st/tiy/budgetopgg/model/domain/Match.java
+++ src/main/java/st/tiy/budgetopgg/model/domain/Match.java
@@ -1,7 +1,10 @@
 package st.tiy.budgetopgg.model.domain;
 
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+
+import java.util.List;
 
 @Entity
 public class Match {
@@ -9,11 +12,22 @@ public class Match {
 	@Id
 	private Long id;
 
+	@ElementCollection
+	private List<String> playersPuuids;
+
 	public void setId(Long id) {
 		this.id = id;
 	}
 
 	public Long getId() {
 		return id;
+	}
+
+	public List<String> getPlayersPuuids() {
+		return playersPuuids;
+	}
+
+	public void setPlayersPuuids(List<String> playersPuuids) {
+		this.playersPuuids = playersPuuids;
 	}
 }
