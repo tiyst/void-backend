e07561230630f4a3d29655725c1c2cc4f6c534c7
tiyst <hello@tiyst.tech>
2025-01-30 01:19:10 +0100

    Additional mapper warnings fix

--- src/main/java/st/tiy/budgetopgg/model/mapper/RiotAccountMapper.java
+++ src/main/java/st/tiy/budgetopgg/model/mapper/RiotAccountMapper.java
@@ -8,6 +8,13 @@ import st.tiy.budgetopgg.model.domain.summoner.Summoner;
 @Mapper(componentModel = "spring")
 public interface RiotAccountMapper {
 
+	@Mapping(target = "accountId", ignore = true)
+	@Mapping(target = "summonerId", ignore = true)
+	@Mapping(target = "server", ignore = true)
+	@Mapping(target = "profileIcon", ignore = true)
+	@Mapping(target = "level", ignore = true)
+	@Mapping(target = "rank", ignore = true)
+	@Mapping(target = "masteries", ignore = true)
 	@Mapping(target = "lastUpdated", expression = "java( System.currentTimeMillis() / 1000L )")
 	Summoner mapAccountDtoToSummoner(RiotAccountDto riotAccountDto);
 
