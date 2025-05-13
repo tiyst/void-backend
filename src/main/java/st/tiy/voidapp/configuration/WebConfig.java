2081c5ba924403676c071b8965b8e10512b171eb
tiyst <hello@tiyst.tech>
2025-05-13 10:18:10 +0200

    Cors config

--- src/main/java/st/tiy/voidapp/configuration/WebConfig.java
+++ src/main/java/st/tiy/voidapp/configuration/WebConfig.java
@@ -1,26 +1,31 @@
 package st.tiy.voidapp.configuration;
 
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
+@Setter
 @Configuration
 @EnableWebMvc
+@ConfigurationProperties(prefix = "void-cors")
 public class WebConfig implements WebMvcConfigurer {
+
+	private List<String> allowedOrigins;
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**")
-		        .allowedOrigins(
-				        "http://localhost:5173",
-				        "https://tiy.st",
-				        "https://*.tiy.st"
-		        )
+		        .allowedOrigins(allowedOrigins.toArray(new String[0]))
 		        .allowedMethods("GET", "POST")
 		        .allowedHeaders("*")
 		        .exposedHeaders("*")
 		        .allowCredentials(true)
 		        .maxAge(3600);
 	}
+
 }
